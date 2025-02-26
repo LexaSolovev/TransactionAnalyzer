370fba42c3b38c914d46e2681c9779c1ee1a87d4
Alexey Solovyev <solovaa@yandex.ru>
2025-02-26 10:58:23 +0000

    add func get_response

--- src/views.py
+++ src/views.py
@@ -1,7 +1,36 @@
-
+import json
+from datetime import datetime
+from utils import *
 
 def get_response(date: str) -> dict:
-    pass
+    """
+    Функция главной страницы, принимает дату в виде строки формата YYYY-MM-DD HH:MM:SS
+    Возвращает ответ в виде словаря
+    """
+    date_obj = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+
+    path_to_excel = os.path.join(PATH_DATA,"operations.xlsx")
+    path_to_user_settings = os.path.join(PATH_DATA, "user_settings.json")
+    transactions_df = get_transactions_df_from_excel(path_to_excel)
+    filtered_trs_by_date = filter_transactions_by_date(transactions_df, date_obj.strftime("%d.%m.%Y"))
+
+    current_date = datetime.now()
+    with open(path_to_user_settings) as f:
+        user_settings = json.load(f)
+        user_currencies = user_settings.get("user_currencies")
+        user_stocks = user_settings.get("user_stocks")
+
+    result = {
+        "greeting": greeting(current_date.strftime("%Y-%m-%d %H:%M:%S")),
+        "cards": get_cards(filtered_trs_by_date),
+        "top_transactions": get_top_transactions(filtered_trs_by_date),
+        "currency_rates": get_currencies_rates(user_currencies),
+        "stock_prices": get_stock_prices(user_stocks)
+    }
+    return result
+
+if __name__ == "__main__":
+    print(get_response("2021-12-30 00:00:00"))
 
 
 
