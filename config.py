790e70308b00f36310ef3846f52f54cd6a1cf0d8
Alexey Solovyev <solovaa@yandex.ru>
2025-02-26 22:08:41 +0000

    add PATH_REPORTS

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 PATH_ROOT = os.path.dirname(__file__)
 PATH_DATA = os.path.join(PATH_ROOT, "data")
 PATH_LOGS = os.path.join(PATH_ROOT, "logs")
+PATH_REPORTS = os.path.join(PATH_ROOT, "reports")
