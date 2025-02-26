1bbd2963b680e7fd6af47653a02a03da3933494c
Alexey Solovyev <solovaa@yandex.ru>
2025-02-26 13:49:14 +0000

    add tests for get_cards and get_top_transactions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,11 @@
-import pytest
+import os.path
+from datetime import datetime
 
-from src.utils import greeting
+import pytest
+from pandas import DataFrame
+
+from config import PATH_DATA
+from src.utils import greeting, get_transactions_df_from_excel, get_cards, get_top_transactions
 
 
 @pytest.mark.parametrize("date, expected", [
@@ -13,3 +18,43 @@ def test_greeting(date, expected):
     assert greeting(date) == expected
 
 
+def test_get_cards(dataframe_for_tests):
+    assert get_cards(dataframe_for_tests) == [
+        {
+            "last_digits": "5091",
+            "total_spent": 564.00,
+            "cashback": 0
+        },
+        {
+          "last_digits": "7197",
+          "total_spent": 224.89,
+          "cashback": 0
+        }
+
+    ]
+
+
+def test_get_top_transactions(dataframe_for_tests):
+    assert get_top_transactions(dataframe_for_tests) == [
+        {
+          "date": "31.12.2021",
+          "amount": 564.00,
+          "category": "Различные товары",
+          "description": "Ozon.ru"
+        },
+        {
+          "date": "31.12.2021",
+          "amount": 160.89,
+          "category": "Супермаркеты",
+          "description": "Колхоз"
+        },
+        {
+            "date": "31.12.2021",
+            "amount": 64.00,
+            "category": "Супермаркеты",
+            "description": "Колхоз"
+        }
+    ]
+
+
+def
