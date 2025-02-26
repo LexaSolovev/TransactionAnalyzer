e79b7521259ee64c9344434f09fc938bdaf5457c
Alexey Solovyev <solovaa@yandex.ru>
2025-02-26 22:09:16 +0000

    add decorator report_to_file

--- src/reports.py
+++ src/reports.py
@@ -1,14 +1,27 @@
 import os
 from datetime import datetime
+from functools import wraps
 from typing import Optional
 
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 
-from config import PATH_DATA
+from config import PATH_DATA, PATH_REPORTS
 from src.utils import filter_transactions_by_date
 
+def report_to_file(file_name :str="default_report"):
+    def inner(func):
+        @wraps(func)
+        def wrapper(*args,**kwargs):
+            result = func(*args, **kwargs)
+            path_to_report = os.path.join(PATH_REPORTS, file_name + ".xlsx")
+            result.to_excel(path_to_report)
+            return result
+        return wrapper
+    return inner
 
+
+@report_to_file("spending_by_category")
 def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
     """
     Функция принимает список транзакций в виде DataFrame, Категорию и дату в формате DD.MM.YYYY
