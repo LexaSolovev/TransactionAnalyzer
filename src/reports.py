7b8794277359d5a1be871f8bd5742f9183a4b5c5
Alexey Solovyev <solovaa@yandex.ru>
2025-02-27 10:56:00 +0000

    add tests for spending_by_category

--- src/reports.py
+++ src/reports.py
@@ -42,7 +42,7 @@ def spending_by_category(transactions: pd.DataFrame, category: str, date: Option
         date_begin.strftime("%d.%m.%Y")
     )
     filter_by_category = filtered_by_date[filtered_by_date["Категория"] == category]
-
+    filter_by_category["Дата операции"] = filter_by_category["Дата операции"].dt.strftime('%d.%m.%Y %H:%M:%S')
     return filter_by_category
 
 
--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,9 +1,10 @@
+import json
 import os
 
 import pytest
 
 from config import PATH_DATA, PATH_REPORTS
-from src.reports import report_to_file
+from src.reports import report_to_file, spending_by_category
 import pandas as pd
 
 def test_report_to_file():
@@ -17,3 +18,27 @@ def test_report_to_file():
     path_to_test_report = os.path.join(PATH_REPORTS, "test_decorator.xlsx")
     df_from_test_decorator = pd.read_excel(path_to_test_report)
     assert test_df.to_json() == df_from_test_decorator.to_json()
+
+
+def test_spending_by_category(dataframe_for_tests):
+    result = spending_by_category(dataframe_for_tests, "Различные товары", "31.12.2021")
+    result_str = result.to_json(orient="records", index=False, date_format="%d.%m.%Y %H:%M:%S", indent=4)
+    result_json = json.loads(result_str)
+    assert result_json == [{
+                "Дата операции": "31.12.2021 01:23:42",
+                "Дата платежа": "31.12.2021",
+                "Номер карты": "*5091",
+                "Статус": "OK",
+                "Сумма операции": -564.00,
+                "Валюта операции": "RUB",
+                "Сумма платежа": -564.00,
+                "Валюта платежа": "RUB",
+                "Кэшбэк": 0,
+                "Категория": "Различные товары",
+                "MCC": 5399,
+                "Описание": "Ozon.ru",
+                "Бонусы(включая кэшбэк)": 5,
+                "Округление на инвесткопилку": 0,
+                "Сумма операции с округлением": 564.00
+            }]
+
