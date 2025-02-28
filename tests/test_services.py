45572430152e9c192ff33cacdae8eb21ba871422
Alexey Solovyev <solovaa@yandex.ru>
2025-02-28 21:49:13 +0000

    fix tests

--- tests/test_services.py
+++ tests/test_services.py
@@ -4,7 +4,6 @@ from src.services import get_best_categories
 
 def test_get_best_categories(json_for_tests):
     assert get_best_categories(json_for_tests, 2021, 12) == {
-        "Различные товары": 564.00,
-        "Супермаркеты": 224.89,
-        "Каршеринг": 7.07
+        "Различные товары": 5,
+        "Супермаркеты": 4,
     }
